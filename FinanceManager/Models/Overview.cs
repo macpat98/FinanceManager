aea454c30b552290e808c1944ef5cbfcf5b382e0
pat.maciejewski98@gmail.com <pat.maciejewski98@gmail.com>
2025-03-18 21:35:50 +0100

    Add models, use nested views

---
+++ FinanceManager/Models/Overview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Models
+{
+    internal class Overview
+    {
+        public decimal Income { get; set; }
+        public decimal Expense { get; set; }
+        public decimal Total { get { return Income - Expense; } }
+    }
+}
---
+++ FinanceManager/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,29 @@
+﻿using FinanceManager.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManager.ViewModels
+{
+    internal class OverviewViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _tekscik = "Hello World!";
+        public Overview Overview { get; set; } = new Overview();
+        public OverviewViewModel()
+        {
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
